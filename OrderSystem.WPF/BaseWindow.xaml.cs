3f1cef9f5000cdd6f69a1a76ffa3f66b3cea96e5
morfanaion <noordzij.am@gmail.com>
2026-04-30 07:25:43 +0200

    Final cleanup

--- OrderSystem.WPF/BaseWindow.xaml.cs
+++ OrderSystem.WPF/BaseWindow.xaml.cs
@@ -12,24 +12,6 @@ namespace OrderSystem.WPF
         public BaseWindow()
         {
             InitializeComponent();
-            PreviewKeyDown += OnPreviewKeyDown;
-        }
-
-        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-            {
-                if(Keyboard.FocusedElement is FrameworkElement element)
-                {
-                    var enumerator = element.GetLocalValueEnumerator();
-                    while (enumerator.MoveNext())
-                    {
-                        LocalValueEntry entry = enumerator.Current;
-                        BindingExpressionBase binding = BindingOperations.GetBindingExpressionBase(element, entry.Property);
-                        binding?.UpdateSource();
-                    }
-                }
-            }
         }
     }
 }
