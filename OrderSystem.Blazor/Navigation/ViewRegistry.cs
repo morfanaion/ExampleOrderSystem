4a31e4bdb59fc5abb67b2cf54d893fe1daead1a7
morfanaion <noordzij.am@gmail.com>
2026-05-15 09:42:57 +0200

    Initial stuff

---
+++ OrderSystem.Blazor/Navigation/ViewRegistry.cs
@@ -0,0 +1,18 @@
+﻿using OrderSystem.Business.ViewModels;
+
+namespace OrderSystem.Blazor.Navigation
+{
+    public static class ViewRegistry
+    {
+        private static readonly Dictionary<Type, Type> _map = new();
+
+        public static void Register<TVm, TComponent>()
+        {
+            _map[typeof(TVm)] = typeof(TComponent);
+        }
+
+        public static Type Resolve(ViewModel vm)
+        {
+            return _map[vm.GetType()];
+        }
+    }
